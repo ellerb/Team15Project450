73b4f5b600db008520eeaccf6e2fa35b557ece6b
Gabrielle <gabriellebly@gmail.com>
2016-06-01 18:56:13 -0700

    Implemented tour details for view created

---
+++ app/src/main/java/tcss450/uw/edu/team15project450/browse/userTour/VCPlaceDetailFragment.java
@@ -0,0 +1,69 @@
+package tcss450.uw.edu.team15project450.browse.userTour;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import tcss450.uw.edu.team15project450.R;
+import tcss450.uw.edu.team15project450.model.Place;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VCPlaceDetailFragment extends Fragment {
+
+    private TextView mPlaceTitle;
+    private TextView mPlaceDescription;
+    private TextView mPlaceLatitude;
+    private TextView mPlaceLongitude;
+
+    public static String PLACE_ITEM_SELECTED = "PlaceItemSelected";
+
+
+    public VCPlaceDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_vcplace_detail, container, false);
+        mPlaceTitle = (TextView) view.findViewById(R.id.vc_place_item_title);
+        mPlaceDescription = (TextView) view.findViewById(R.id.vc_place_item_desc);
+        mPlaceLatitude = (TextView) view.findViewById(R.id.vc_place_item_latitude);
+        mPlaceLongitude = (TextView) view.findViewById(R.id.vc_place_item_longitude);
+
+        return view;
+    }
+
+    public void updateView(Place place) {
+        if (place != null) {
+            mPlaceTitle.setText(place.getTitle());
+            mPlaceDescription.setText(place.getDescription());
+            //mPlaceLatitude.setText(place.getLatitude());
+            //mPlaceLongitude.setText(place.getLongitude());
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method
+        // below that sets the article text.
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            updateView((Place) args.getSerializable(PLACE_ITEM_SELECTED));
+        }
+    }
+
+}
---
+++ app/src/main/java/tcss450/uw/edu/team15project450/browse/userTour/VCPlaceFragment.java
@@ -0,0 +1,112 @@
+package tcss450.uw.edu.team15project450.browse.userTour;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import tcss450.uw.edu.team15project450.R;
+import tcss450.uw.edu.team15project450.browse.userTour.MyVCPlaceRecyclerViewAdapter;
+import tcss450.uw.edu.team15project450.model.Place;
+import tcss450.uw.edu.team15project450.model.Tour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ * <p>
+ * Activities containing this fragment MUST implement the {@link OnPlaceListFragmentInteractionListener}
+ * interface.
+ */
+public class VCPlaceFragment extends Fragment {
+
+    private int mColumnCount = 1;
+    private RecyclerView mRecyclerView;
+    private OnPlaceListFragmentInteractionListener mListener;
+
+    public static String TOUR_ITEM_SELECTED = "TourItemSelected";
+    ArrayList<Place> placeList = new ArrayList<>();
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public VCPlaceFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_place_list_vc, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            mRecyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                mRecyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+        }
+
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            getPlaceList((Tour) args.getSerializable(TOUR_ITEM_SELECTED));
+            mRecyclerView.setAdapter(new MyVCPlaceRecyclerViewAdapter(placeList, mListener));
+        }
+        return view;
+    }
+
+    public ArrayList<Place> getPlaceList(Tour tour) {
+        //get place
+        if (tour != null && !tour.getPlaceList().isEmpty()) {
+            placeList = tour.getPlaceList();
+        }
+        return placeList;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnPlaceListFragmentInteractionListener) {
+            mListener = (OnPlaceListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnPlaceListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnPlaceListFragmentInteractionListener {
+        void onPlaceListFragmentInteraction(Place item);
+    }
+}
